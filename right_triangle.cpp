50f7fc89d389984d370e1dd5be48c76c551e533e
Chaotic Good Barbarian <tazdingodan@gmail.com>
2025-11-21 12:00:53 +0200

    finished

--- right_triangle.cpp
+++ right_triangle.cpp
@@ -4,5 +4,13 @@ int main() {
 	int num1, num2, num3;
 	std::cin >> num1 >> num2 >> num3;
 
-	
+	if ((num3 * num3 == (num1 * num1) + (num2 * num2)) ||
+		(num2 * num2 == (num1 * num1) + (num3 * num3)) ||
+		(num1 * num1 == (num2 * num2) + (num3 * num3))) {
+		std::cout << "YES";
+	}
+	else {
+		std::cout << "NO";
+	}
+	return 0;
 }
